c33423363d6558896c7f848d6b0ec46824be093c
miyucy <miyucy@gmail.com>
2010-09-22 01:53:52 +0900

    changed build method of swfmill

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -100,35 +100,88 @@ have_library("stdc++")
 
 $INCFLAGS += " -I./swfmill/src -I./swfmill/src/swft -I./swfmill/src/xslt -I./libb64/include"
 
-Dir.chdir("swfmill") do
-  cmd = <<CMD
-sh configure --config-cache --disable-dependency-tracking \
-FREETYPE_CFLAGS="#{$FREETYPE_CFLAGS}" FREETYPE_LIBS="#{$FREETYPE_LIBS}" \
-XML_CFLAGS="#{$XML_CFLAGS}" XML_LIBS="#{$XML_LIBS}" \
-XSLT_CFLAGS="#{$XSLT_CFLAGS}" XSLT_LIBS="#{$XSLT_LIBS}" \
-PNG_CFLAGS="#{$PNG_CFLAGS}" PNG_LIBS="#{$PNG_LIBS}" \
---with-pic \
---enable-shared \
-CFLAGS="-fPIC -O3" \
-CXXFLAGS="-fPIC -O3 -finline -finline-functions"
-CMD
-  raise "error: #{cmd}" unless system(cmd)
-  cmd = "make"
-  raise "error: #{cmd}" unless system(cmd)
-end
-
 Dir.chdir("libb64") do
   cmd = "make"
   raise "error: #{cmd}" unless system(cmd)
 end
 
-create_makefile("swfmill_ext")
-File.open(File.dirname("__FILE__") + "/Makefile", "ab") do |f|
-  exclude = ['swfmill/src/swfmill-base64.o']
-  objects = Dir["swfmill/src/**/*.o"] - exclude
-  f.puts "OBJS += #{objects.join(" ")}"
+Dir.chdir("swfmill/src") do
+  cmd = "xsltproc codegen/mk.xsl codegen/source.xml"
+  raise "error: #{cmd}" unless system(cmd)
+
+  namespaces  = 'xmlns:swft="http://subsignal.org/swfml/swft" '
+  namespaces << 'xmlns:str="http://exslt.org/strings" '
+  namespaces << 'xmlns:math="http://exslt.org/math"'
+
+  simple = %x"xsltproc xslt/assemble.xsl xslt/simple.xml"
+  File.open("simple.cpp", "wb") do |f|
+    f.puts '#include "xslt/xslt.h"'
+    f.puts 'const char *xslt_simple = '
+    f.puts simple.sub(/namespaces="hack"/, namespaces).gsub(/"/, '\"').gsub(/^.*$/){ |m| '"' + m + '\n"' }
+    f.puts ';'
+  end
 end
+
+create_makefile("swfmill_ext")
 
 File.open(File.dirname("__FILE__") + "/Makefile", "ab") do |f|
   f.puts "OBJS += libb64/src/cdecode.o libb64/src/cencode.o"
 end
+
+swfmill_files = <<FILES.split.map{|file| file.chomp}
+swfmill/src/gSWFBasics.cpp
+swfmill/src/gSWFDumper.cpp
+swfmill/src/gSWFParseXML.cpp
+swfmill/src/gSWFParser.cpp
+swfmill/src/gSWFSize.cpp
+swfmill/src/gSWFWriteXML.cpp
+swfmill/src/gSWFWriter.cpp
+swfmill/src/Geom.cpp
+swfmill/src/SWFAction.cpp
+swfmill/src/SWFFile.cpp
+swfmill/src/SWFFilter.cpp
+swfmill/src/SWFGlyphList.cpp
+swfmill/src/SWFItem.cpp
+swfmill/src/SWFReader.cpp
+swfmill/src/SWFShapeItem.cpp
+swfmill/src/SWFShapeMaker.cpp
+swfmill/src/SWFTag.cpp
+swfmill/src/SWFTrait.cpp
+swfmill/src/SWFWriter.cpp
+swfmill/src/simple.cpp
+swfmill/src/swft/Parser.cpp
+swfmill/src/swft/SVGAttributeParser.cpp
+swfmill/src/swft/SVGColor.cpp
+swfmill/src/swft/SVGGradient.cpp
+swfmill/src/swft/SVGPathParser.cpp
+swfmill/src/swft/SVGPointsParser.cpp
+swfmill/src/swft/SVGStyle.cpp
+swfmill/src/swft/SVGTransformParser.cpp
+swfmill/src/swft/readpng.c
+swfmill/src/swft/swft.cpp
+swfmill/src/swft/swft_document.cpp
+swfmill/src/swft/swft_import.cpp
+swfmill/src/swft/swft_import_binary.cpp
+swfmill/src/swft/swft_import_jpeg.cpp
+swfmill/src/swft/swft_import_mp3.cpp
+swfmill/src/swft/swft_import_png.cpp
+swfmill/src/swft/swft_import_ttf.cpp
+swfmill/src/swft/swft_import_wav.cpp
+swfmill/src/swft/swft_path.cpp
+FILES
+
+data = File.open(File.dirname("__FILE__") + "/Makefile", "rb"){ |f| f.read }
+data.gsub!(/^OBJS = .*\n/) do |m|
+  objs = swfmill_files.map{ |e| File.join(File.dirname(e), File.basename(e, ".*") + ".o") }
+  m << "OBJS+= " << objs.join(" ") << "\n"
+end
+swfmill_files.each do |e|
+  o = File.join(File.dirname(e), File.basename(e, ".*") + ".o")
+  data << "#{o}: #{e}\n"
+  if e =~ /cpp\Z/
+    data << "\t$(CXX) $(INCFLAGS) $(CPPFLAGS) $(CXXFLAGS) -o $@ -c $<\n"
+  else
+    data << "\t$(CC) $(INCFLAGS) $(CPPFLAGS) $(CFLAGS) -o $@ -c $<\n"
+  end
+end
+File.open(File.dirname("__FILE__") + "/Makefile", "wb") { |f| f.write data }
